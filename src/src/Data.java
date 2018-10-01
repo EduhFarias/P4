7c99fff9a759a27eb670fe9cd0ca10f1c889cfe3
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-09-30 22:21:18 -0300

    Update Data.java

--- src/src/Data.java
+++ src/src/Data.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package src;
 
 /**
  *
- * @author PC
+ * @author @EduhFarias
  */
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
